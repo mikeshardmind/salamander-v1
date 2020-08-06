2546732646444720f6b747b263565a58247df04d
Michael H <michael@michaelhall.tech>
2020-08-06 11:37:48 -0400

    Awaiting discussion on continuation

---
+++ src/ipc_layer.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+"""
+Module for handling the IPC abstractions
+
+Needs a few more design discussions, but the general need
+is just a basic messaging queue right now.
+"""
+
+
+async def run_zmq():
+    ...  # TODO: actually, maybe this should be in a thread...
