21904921020db14f91c37c91c1a04de99db54744
Michael H <michael@michaelhall.tech>
2020-08-17 01:38:53 -0400

    uuid4 doesn't fit in a c-long

--- src/bot.py
+++ src/bot.py
@@ -165,7 +165,7 @@ class Salamander(commands.Bot):
         but if anything blocks the loop for that long, there are larger issues.
         """
 
-        this_uuid = uuid4().int
+        this_uuid = uuid4().hex
 
         def matches(*args) -> bool:
             topic, (recv_uuid, *_data) = args
