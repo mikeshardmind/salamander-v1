f9fb5db98c9c56c32008280343b819bc44d83398
Michael H <michael@michaelhall.tech>
2020-08-29 17:54:00 -0400

    add some basic basic testing

--- src/cogs/filt.py
+++ src/cogs/filt.py
@@ -7,9 +7,13 @@ import discord
 from discord.ext import commands
 
 if TYPE_CHECKING:
-    from ..bot import Salamander
+    from ..bot import Salamander, SalamanderContext
 else:
     Salamander = commands.Bot
+    SalamanderContext = commands.Context
+
+
+REFOCUS = "basalisk.refocus"
 
 
 class FilterDemo(commands.Cog):
@@ -25,6 +29,18 @@ class FilterDemo(commands.Cog):
         if msg.content and (not msg.author.bot):
 
             if await self.bot.check_basalisk(msg.content):
-                await msg.channel.send("Found link...")
+                await msg.channel.send("Found match")
 
-    
+    @commands.is_owner()
+    @commands.command()
+    async def addpattern(self, ctx: SalamanderContext, pattern):
+        """ Add a pattern to the scanner """
+        self.bot.ipc_put(REFOCUS, ((pattern,), ()))
+        await ctx.send("Pattern added.")
+
+    @commands.is_owner()
+    @commands.command()
+    async def removepattern(self, ctx: SalamanderContext, pattern):
+        """ Remove a pattern from the scanner """
+        self.bot.ipc_put(REFOCUS, ((), (pattern,)))
+        await ctx.send("Pattern removed.")
