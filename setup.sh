f4bdf9e4f88ac2ca33b6f69cc21029c8590a942e
Michael H <michael@michaelhall.tech>
2020-09-30 11:35:56 -0400

    setup script

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+sqlite3 salamander.sqlite < schema.sql
+python3.8 -m pip install -r requirements.txt
+python3.8 -m pip install https://github.com/rogerbinns/apsw/releases/download/3.33.0-r1/apsw-3.33.0-r1.zip \
+    --global-option=fetch --global-option=--version --global-option=3.33.0 --global-option=--all \
+    --global-option=build --global-option=--enable-all-extensions
