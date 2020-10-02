2d79321d65847bd44f7e034a87d9fc46f47a6b4d
Michael H <michael@michaelhall.tech>
2020-10-02 04:08:33 -0400

    Put correct data in discord reason

--- src/extensions/mod/__init__.py
+++ src/extensions/mod/__init__.py
@@ -11,3 +11,9 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
+
+from .core import Mod
+
+
+def setup(bot):
+    bot.add_cog(Mod(bot))
