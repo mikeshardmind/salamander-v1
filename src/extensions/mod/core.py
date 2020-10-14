06407d0a969eafbf43c75cbb12884e168ca13527
Michael H <michael@michaelhall.tech>
2020-10-14 08:14:37 -0400

    Timed mute support

--- src/extensions/mod/core.py
+++ src/extensions/mod/core.py
@@ -15,8 +15,10 @@
 
 from __future__ import annotations
 
-from datetime import datetime
-from typing import Optional
+import asyncio
+from collections import defaultdict
+from datetime import datetime, timezone
+from typing import Dict, List, Optional
 
 import discord
 from discord.ext import commands
@@ -24,6 +26,7 @@ from discord.ext import commands
 from ...bot import HierarchyException, Salamander, SalamanderContext, UserFeedbackError
 from ...checks import admin_or_perms, mod_or_perms
 from ...utils import format_list
+from ...utils.converters import MemberOrID, TimedeltaConverter
 
 INSERT_OR_IGNORE_GUILD = """
 INSERT INTO guild_settings (guild_id) VALUES (?)
@@ -173,6 +176,7 @@ class Mod(commands.Cog):
 
     def __init__(self, bot: Salamander):
         self.bot: Salamander = bot
+        self._mute_locks: Dict[int, asyncio.Lock] = defaultdict(asyncio.Lock)
 
     @mod_or_perms(kick_members=True)
     @commands.bot_has_guild_permissions(kick_members=True)
@@ -192,15 +196,28 @@ class Mod(commands.Cog):
     @commands.bot_has_guild_permissions(manage_roles=True)
     @commands.command(name="ban")
     async def ban_command(
-        self, ctx: SalamanderContext, who: discord.Member, *, reason: str = "",
+        self, ctx: SalamanderContext, who: MemberOrID, *, reason: str = "",
     ):
         """ Ban a member without removing messages """
 
-        ban_sanity_check(bot_user=ctx.me, mod=ctx.author, target=who)
-        await who.ban(
-            reason=f"User banned by command. (Authorizing mod: {ctx.author}({ctx.author.id})"
-        )
-        self.bot.modlog.member_ban(mod=ctx.author, target=who, reason=reason)
+        if member := who.member:
+
+            ban_sanity_check(bot_user=ctx.me, mod=ctx.author, target=member)
+            await member.ban(
+                reason=f"User banned by command. (Authorizing mod: {ctx.author}({ctx.author.id})",
+                delete_message_days=0,
+            )
+
+        else:
+            drsn = (
+                f"User not in guild banned by command. "
+                f"(Authorizing mod: {ctx.author}({ctx.author.id})"
+            )
+            await ctx.guild.ban(
+                discord.Object(who.id), reason=drsn, delete_message_days=0
+            )
+
+        self.bot.modlog.member_ban(mod=ctx.author, target=member, reason=reason)
 
     # TODO: more commands / ban options
 
@@ -213,63 +230,82 @@ class Mod(commands.Cog):
         audit_reason: str,
         expiration: Optional[datetime] = None,
     ):
-
         guild = mod.guild
+        async with self._mute_locks[guild.id]:
+
+            cursor = self.bot._conn.cursor()
+            params = (guild.id,)
+            cursor.execute(INSERT_OR_IGNORE_GUILD, params)
+
+            row = cursor.execute(GET_MUTE_ROLE, params).fetchone()
+
+            mute_role_id = next(iter(row), None) if row is not None else None
+
+            if mute_role_id is None:
+                raise UserFeedbackError(
+                    custom_message="No mute role has been configured."
+                )
+
+            mute_role = guild.get_role(mute_role_id)
+            if mute_role is None:
+                raise UserFeedbackError(
+                    custom_message="The mute role for this server appears to have been deleted."
+                )
+
+            mute_sanity_check(bot_user=guild.me, mod=mod, target=target)
+
+            removed_role_ids = []
+            intended_state = [mute_role]
+            for r in target.roles:
+                if r.managed or r.is_default():
+                    intended_state.append(r)
+                else:
+                    removed_role_ids.append(r.id)
+
+            intended_state.sort()
+
+            if intended_state != target.roles:
+                await target.edit(roles=intended_state, reason=audit_reason)
+
+            self.bot.modlog.member_muted(mod=mod, target=target, reason=reason)
+
+            with self.bot._conn:
+                expirestamp = expiration.isoformat() if expiration is not None else None
+                cursor.execute(
+                    """
+                    INSERT INTO guild_mutes (guild_id, user_id, mute_role_used, expires_at)
+                    VALUES (?, ?, ?, ?)
+                    ON CONFLICT (guild_id, user_id) DO UPDATE SET
+                        mute_role_used=excluded.mute_role_used,
+                        expires_at=excluded.expires_at
+                    """,
+                    (guild.id, target.id, mute_role_id, expirestamp),
+                )
+                cursor.executemany(
+                    """
+                    INSERT INTO guild_mute_removed_roles (guild_id, user_id, removed_role_id)
+                    VALUES (?,?,?)
+                    ON CONFLICT (guild_id, user_id, removed_role_id)
+                    DO NOTHING
+                    """,
+                    tuple((guild.id, target.id, rid) for rid in removed_role_ids),
+                )
+
+    async def background_loop(self):
+
         cursor = self.bot._conn.cursor()
-        params = (guild.id,)
-        cursor.execute(INSERT_OR_IGNORE_GUILD, params)
 
-        row = cursor.execute(GET_MUTE_ROLE, params).fetchone()
+        while True:
 
-        mute_role_id = next(iter(row), None) if row is not None else None
-
-        if mute_role_id is None:
-            raise UserFeedbackError(custom_message="No mute role has been configured.")
-
-        mute_role = guild.get_role(mute_role_id)
-        if mute_role is None:
-            raise UserFeedbackError(
-                custom_message="The mute role for this server appears to have been deleted."
-            )
-
-        if mute_role in target.roles:
-            raise UserFeedbackError(custom_message="User is already muted.")
-
-        mute_sanity_check(bot_user=guild.me, mod=mod, target=target)
-
-        removed_role_ids = []
-        intended_state = [mute_role]
-        for r in target.roles:
-            if r.managed or r.is_default():
-                intended_state.append(r)
-            else:
-                removed_role_ids.append(r.id)
-
-        await target.edit(roles=intended_state, reason=audit_reason)
-
-        self.bot.modlog.member_muted(mod=mod, target=target, reason=reason)
-
-        with self.bot._conn:
-            expirestamp = expiration.isoformat() if expiration is not None else None
-            cursor.execute(
+            for guild_id, user_id in cursor.execute(
                 """
-                INSERT INTO guild_mutes (guild_id, user_id, mute_role_used, expires_at)
-                VALUES (?, ?, ?, ?)
-                ON CONFLICT (guild_id, user_id) DO UPDATE SET
-                    mute_role_used=excluded.mute_role_used,
-                    expires_at=excluded.expires_at
-                """,
-                (guild.id, target.id, mute_role_id, expirestamp),
-            )
-            cursor.executemany(
+                SELECT guild_id, user_id FROM guild_mutes
+                WHERE expires_at IS NOT NULL and expires_at < CURRENT_TIMESTAMP
                 """
-                INSERT INTO guild_mute_removed_roles (guild_id, user_id, removed_role_id)
-                VALUES (?,?,?)
-                ON CONFLICT (guild_id, user_id, removed_role_id)
-                DO NOTHING
-                """,
-                tuple((guild.id, target.id, rid) for rid in removed_role_ids),
-            )
+            ):
+                asyncio.create_task(
+                    self.unmute_logic(guild_id, user_id, "", "Tempmute expiration")
+                )
 
     @mod_or_perms(manage_roles=True)
     @commands.bot_has_guild_permissions(manage_roles=True)
@@ -288,14 +324,63 @@ class Mod(commands.Cog):
 
     @mod_or_perms(manage_roles=True)
     @commands.bot_has_guild_permissions(manage_roles=True)
+    @commands.command(name="tempmute")
+    async def temp_mute_command(
+        self,
+        ctx: SalamanderContext,
+        who: discord.Member,
+        duration: TimedeltaConverter,
+        *,
+        reason: str = "",
+    ):
+        """ Mute a user using the configure mute role for a duration. """
+
+        audit_reason = (
+            f"User muted for {duration} by command. (Mod: {ctx.author}({ctx.author.id})"
+        )
+
+        expiration = datetime.utcnow().replace(tzinfo=timezone.utc) + duration.delta
+
+        await self.mute_user_logic(
+            mod=ctx.author,
+            target=who,
+            reason=reason,
+            audit_reason=audit_reason,
+            expiration=expiration,
+        )
+        await ctx.send(f"User Muted for {duration}")
+
+    @mod_or_perms(manage_roles=True)
+    @commands.bot_has_guild_permissions(manage_roles=True)
     @commands.command(name="unmute")
     async def basic_unmute_command(
         self, ctx: SalamanderContext, who: discord.Member, *, reason: str = "",
     ):
         """ Unmute a user """
 
-        cursor = ctx.bot._conn.cursor()
-        params = (ctx.guild.id,)
+        audit_reason = f"User unmuted by command. (Mod: {ctx.author}({ctx.author.id})"
+        cant_restore = await self.unmute_logic(
+            ctx.guild.id, who.id, reason, audit_reason, mod=ctx.author
+        )
+
+        if cant_restore:
+            r_s = format_list([r.name for r in cant_restore])
+            await ctx.send(f"User unmuted. A few roles could not be restored: {r_s}")
+        else:
+            await ctx.send("User unmuted.")
+
+    async def unmute_logic(
+        self,
+        guild_id: int,
+        member_id: int,
+        reason: str,
+        audit_reason: str,
+        *,
+        mod: Optional[discord.Member] = None,
+    ) -> List[discord.Role]:
+
+        cursor = self.bot._conn.cursor()
+        params = (guild_id,)
         cursor.execute(INSERT_OR_IGNORE_GUILD, params)
 
         row = cursor.execute(GET_MUTE_ROLE, params).fetchone()
@@ -304,73 +389,98 @@ class Mod(commands.Cog):
         if mute_role_id is None:
             raise UserFeedbackError(custom_message="No mute role has been configured.")
 
-        mute_role = ctx.guild.get_role(mute_role_id)
+        guild = self.bot.get_guild(guild_id)
+        if not guild:
+            raise RuntimeError()  # only possible from bg loop
+
+        mute_role = guild.get_role(mute_role_id)
         if mute_role is None:
             raise UserFeedbackError(
                 custom_message="The mute role for this server appears to have been deleted."
             )
 
-        if mute_role not in who.roles:
-            raise UserFeedbackError(custom_message="User does not appear to be muted")
+        who = guild.get_member(member_id)
 
-        member_params = (ctx.guild.id, who.id)
+        member_params = (guild_id, member_id)
 
-        if (
+        if who is None:
+            async with self._mute_locks[guild_id]:
+                cursor.execute(
+                    """
+                    DELETE FROM guild_mutes WHERE guild_id = ? AND user_id = ?
+                    """,
+                    member_params,
+                )
+            raise UserFeedbackError(custom_message="User is no longer in this server.")
+
+        elif mute_role not in who.roles:
+            async with self._mute_locks[guild_id]:
+                cursor.execute(
+                    """
+                    DELETE FROM guild_mutes WHERE guild_id = ? AND user_id = ?
+                    """,
+                    member_params,
+                )
+                # Prevent mute dodge applying if someone manually unmuted then this command was run
+                raise UserFeedbackError(
+                    custom_message="User does not appear to be muted."
+                )
+
+        async with self._mute_locks[guild_id]:
+            if (
+                cursor.execute(
+                    """
+                    SELECT 1
+                    FROM guild_mutes
+                    WHERE guild_id=? AND user_id=?
+                    """,
+                    member_params,
+                ).fetchone()
+                is None
+            ):
+                raise UserFeedbackError(
+                    custom_message="User was not muted using this bot (not unmuting)."
+                )
+
+            # Above is needed since it is possible to mute someone *without* removing any roles
+
+            to_restore = [
+                role_id
+                for (role_id,) in cursor.execute(
+                    """
+                    SELECT removed_role_id
+                    FROM guild_mute_removed_roles
+                    WHERE guild_id=? AND user_id=?
+                    """,
+                    member_params,
+                )
+            ]
+
+            intended_state = [r for r in who.roles if r.id != mute_role_id]
+            cant_add = []
+            for role_id in to_restore:
+                role = guild.get_role(role_id)
+                if role:
+                    if role.managed or role >= guild.me.top_role:
+                        cant_add.append(role)
+                    else:
+                        intended_state.append(role)
+
+            intended_state.sort()
+
+            if intended_state != who.roles:
+                await who.edit(roles=intended_state, reason=audit_reason)
+
+            if mod:
+                self.bot.modlog.member_unmuted(mod=mod, target=who, reason=reason)
+
             cursor.execute(
                 """
-                SELECT 1
-                FROM guild_mutes
-                WHERE guild_id=? AND user_id=?
-                """,
-                member_params,
-            ).fetchone()
-            is None
-        ):
-            raise UserFeedbackError(
-                custom_message="User was not muted using this bot (not unmuting)."
-            )
-
-        # Above is needed since it is possible to mute someone *without* removing any roles
-
-        to_restore = [
-            role_id
-            for (role_id,) in cursor.execute(
-                """
-                SELECT removed_role_id
-                FROM guild_mute_removed_roles
-                WHERE guild_id=? AND user_id=?
+                DELETE FROM guild_mutes WHERE guild_id = ? AND user_id = ?
                 """,
                 member_params,
             )
-        ]
-
-        intended_state = [r for r in who.roles if r.id != mute_role_id]
-        cant_add = []
-        for role_id in to_restore:
-            role = ctx.guild.get_role(role_id)
-            if role:
-                if role.managed or role >= ctx.guild.me.top_role:
-                    cant_add.append(role)
-                else:
-                    intended_state.append(role)
-
-        audit_reason = f"User unmuted by command. (Mod: {ctx.author}({ctx.author.id})"
-
-        await who.edit(roles=intended_state, reason=audit_reason)
-
-        self.bot.modlog.member_unmuted(mod=ctx.author, target=who, reason=reason)
-        cursor.execute(
-            """
-            DELETE FROM guild_mutes WHERE guild_id = ? AND user_id = ?
-            """,
-            (ctx.guild.id, who.id),
-        )
-
-        if cant_add:
-            r_s = format_list([r.name for r in cant_add])
-            await ctx.send(f"User unmuted. A few roles could not be restored: {r_s}")
-        else:
-            await ctx.send("User unmuted.")
+            return cant_add
 
     @admin_or_perms(manage_roles=True)
     @commands.bot_has_guild_permissions(manage_roles=True)
@@ -440,41 +550,42 @@ class Mod(commands.Cog):
         if not guild.me.guild_permissions.manage_roles:
             return  # Can't do anything anyhow
 
-        if (
+        async with self._mute_locks[guild.id]:
+            if (
+                cursor.execute(
+                    """
+                    SELECT 1
+                    FROM guild_mutes
+                    WHERE guild_id=? AND user_id=?
+                    """,
+                    (guild.id, member.id),
+                ).fetchone()
+                is None
+            ):
+                return
+
+            # check that we have a mute role
+
+            row = cursor.execute(GET_MUTE_ROLE, (guild.id,)).fetchone()
+            if row is None:
+                return
+
+            (role_id,) = row
+
+            role = guild.get_role(role_id)
+            if role is None:
+                return
+
+            if role > guild.me.top_role and guild.owner != guild.me:
+                return
+
+            # Prevents unexpected granting of roles to mute dodgers
             cursor.execute(
                 """
-                SELECT 1
-                FROM guild_mutes
-                WHERE guild_id=? AND user_id=?
+                DELETE FROM guild_mute_removed_roles
+                WHERE guild_id = ? AND user_id = ?
                 """,
                 (guild.id, member.id),
-            ).fetchone()
-            is None
-        ):
-            return
+            )
 
-        # check that we have a mute role
-
-        row = cursor.execute(GET_MUTE_ROLE, (guild.id,)).fetchone()
-        if row is None:
-            return
-
-        (role_id,) = row
-
-        role = guild.get_role(role_id)
-        if role is None:
-            return
-
-        if role > guild.me.top_role and guild.owner != guild.me:
-            return
-
-        # Prevents unexpected granting of roles to mute dodgers
-        cursor.execute(
-            """
-            DELETE FROM guild_mute_removed_roles
-            WHERE guild_id = ? AND user_id = ?
-            """,
-            (guild.id, member.id),
-        )
-
-        await member.add_roles(role, reason="Detected mute dodge on rejoin")
+            await member.add_roles(role, reason="Detected mute dodge on rejoin")
