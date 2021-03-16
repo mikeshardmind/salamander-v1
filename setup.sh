d5bae085d559baf4d3fdf1bb6d9c8a1b242ac81f
Michael H <michael@michaelhall.tech>
2021-03-16 07:44:35 -0400

    [DEP UPDATE] APSW+SQLite 3.35

--- setup.sh
+++ setup.sh
@@ -6,12 +6,12 @@ sqlite3 salamander.db < schema.sql
 python3.8 -m pip install -U wheel setuptools pip
 python3.8 -m pip install -Ur requirements.txt --upgrade-strategy eager
 
-if [ ! -f wheels/apsw-3.34.1.post1-cp38-cp38*.whl ]; then
+if [ ! -f wheels/apsw-3.35.0.post1-cp38-cp38*.whl ]; then
   git clone https://github.com/rogerbinns/apsw/
   pushd apsw
   git config --local user.email "nobody@example.org"
   git config --local user.name "nobody"
-  git checkout b292f5bae82aa593ccd7710fc5b22f67dd1f68bd
+  git checkout 8a4858dad1dfd5d35bfd30c445641923f1a274e4
   git am ../patches/apsw/*.patch
   python setup.py fetch --all
   python setup.py bdist_wheel
@@ -20,4 +20,4 @@ if [ ! -f wheels/apsw-3.34.1.post1-cp38-cp38*.whl ]; then
   rm -rf apsw
 fi
 
-python3.8 -m pip install wheels/apsw-3.34.1.post1-cp38-cp38*.whl
+python3.8 -m pip install wheels/apsw-3.35.0.post1-cp38-cp38*.whl
