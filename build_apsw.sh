1a98fb5091f60d9182dd4ecc625e4302632cfa4c
Michael H <michael@michaelhall.tech>
2021-04-02 13:56:09 -0400

    more updates

---
+++ build_apsw.sh
@@ -0,0 +1,13 @@
+if [ ! -f wheels/apsw-3.35.2.post1-cp38-cp38*.whl ]; then
+  git clone https://github.com/rogerbinns/apsw/
+  pushd apsw
+  git config --local user.email "nobody@example.org"
+  git config --local user.name "nobody"
+  git checkout 3d9c51f09258ab5140fca05c651c66899c608759
+  git am ../patches/apsw/*.patch
+  python setup.py fetch --all --version=3.35.3
+  python setup.py bdist_wheel
+  cp dist/* ../wheels/
+  popd
+  rm -rf apsw
+fi
--- setup.sh
+++ setup.sh
@@ -4,19 +4,5 @@ mkdir -p wheels
 sqlite3 salamander.db < schema.sql
 python3.8 -m pip install -U wheel setuptools pip
 python3.8 -m pip install -Ur requirements.txt --upgrade-strategy eager
-
-if [ ! -f wheels/apsw-3.35.2.post1-cp38-cp38*.whl ]; then
-  git clone https://github.com/rogerbinns/apsw/
-  pushd apsw
-  git config --local user.email "nobody@example.org"
-  git config --local user.name "nobody"
-  git checkout 3d9c51f09258ab5140fca05c651c66899c608759
-  git am ../patches/apsw/*.patch
-  python setup.py fetch --all --version=3.35.3
-  python setup.py bdist_wheel
-  cp dist/* ../wheels/
-  popd
-  rm -rf apsw
-fi
-
+./build_apsw.sh
 python3.8 -m pip install wheels/apsw-3.35.2.post1-cp38-cp38*.whl
