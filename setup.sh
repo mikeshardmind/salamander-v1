d5580297cd444cc15df7d0dde367c0d219d9d6b0
Michael H <michael@michaelhall.tech>
2021-04-02 17:03:11 -0400

    use apsw to handle db setup as well

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 mkdir -p wheels
-sqlite3 salamander.db < schema.sql
 python3.8 -m pip install -U wheel setuptools pip
 python3.8 -m pip install -Ur requirements.txt --upgrade-strategy eager
 ./build_apsw.sh
 python3.8 -m pip install wheels/apsw-3.35.2.post1-cp38-cp38*.whl
+printf ".open salamander.db\n.read schema.sql\n.quit" | python3.8 -c "import apsw; apsw.main()"
