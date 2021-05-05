1ea3f25870a6e423c434c54ed26c2eafedef156f
Michael H <michael@michaelhall.tech>
2021-05-04 20:36:33 -0400

    Make this work with the stuff in VC

--- build_apsw.sh
+++ build_apsw.sh
@@ -7,8 +7,8 @@ if [ ! -f wheels/apsw-3.35.4.post1-cp38-cp38*.whl ]; then
   git config --local user.name "nobody"
   git checkout d52777bec644c45bf416f7cc02c483fbe53e45fd
   git am ../patches/apsw/*.patch
-  python setup.py fetch --all --version=3.35.4
-  python setup.py bdist_wheel
+  python3.8 setup.py fetch --all --version=3.35.4
+  python3.8 setup.py bdist_wheel
   cp dist/* ../wheels/
   popd
   rm -rf apsw
