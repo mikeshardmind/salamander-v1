54df63366e1e49bb92b84ed861b13d688c2c551c
Michael H <michael@michaelhall.tech>
2022-02-14 02:18:19 -0500

    remove unneeded line

--- setup.sh
+++ setup.sh
@@ -4,6 +4,5 @@ mkdir -p wheels
 python3.9 -m pip install -U wheel setuptools pip
 python3.9 -m pip install --require-hashes -Ur requirements.txt --upgrade-strategy eager
 ./build_apsw.sh
-python3.9 -m pip install wheels/apsw-3.35.4.post1-cp39-cp39*.whl
 printf ".open salamander.db\n.read schema.sql\n.quit" | python3.9 -c "import apsw; apsw.main()"
 exit 0
