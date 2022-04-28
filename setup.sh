043d14eb0d8f409bc1fa1e29a6508ea424f0f697
Michael H <michael@michaelhall.tech>
2022-04-28 04:59:02 -0400

    type fox

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p wheels
-python3.19 -m pip install -U wheel setuptools pip
+python3.10 -m pip install -U wheel setuptools pip
 python3.10 -m pip install requirements-lax.txt --upgrade-strategy eager
 ./build_apsw.sh
 printf ".open salamander.db\n.read schema.sql\n.quit" | python3.10 -c "import apsw; apsw.main()"
