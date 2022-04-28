1c7a76375042f5bdb72f880cbd128d819dde8765
Michael H <michael@michaelhall.tech>
2022-04-28 05:00:08 -0400

    Have this actually upgrade versions

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 mkdir -p wheels
 python3.10 -m pip install -U wheel setuptools pip
-python3.10 -m pip install requirements-lax.txt --upgrade-strategy eager
+python3.10 -m pip install -U -r requirements-lax.txt --upgrade-strategy eager
 ./build_apsw.sh
 printf ".open salamander.db\n.read schema.sql\n.quit" | python3.10 -c "import apsw; apsw.main()"
 exit 0
